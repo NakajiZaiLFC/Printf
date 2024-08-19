4baeae65193a32f4909b87b4b1d6e53309dd7419
NakajiZaiLFC <snakajim@student.42tokyo.jp>
2024-08-20 08:10:36 +0900

    Change how to treat va_list

--- Printf/main.c
+++ Printf/main.c
@@ -9,5 +9,7 @@ int main(void)
 {
     printf("pritf:%c%c%c\n", 'a', '\t', 'c');
     ft_printf("ft_printf:%c%c%c\n", 'a', '\t', 'c');
+    printf("pritf:%c%c%c\n", '1', '2', '3');
+	ft_printf("ft_printf:%c%c%c\n", '1', '2', '3');
     return (0);
 }
